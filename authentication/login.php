4bb0dd0bbd3a4131279ac789149bde7659532911
mayur rahangdale <rahangdalemayur@gmail.com>
2019-02-15 22:26:16 +0530

    changed project structure

---
+++ authentication/login.php
@@ -0,0 +1,22 @@
+<?php 
+
+// include_once 'default-header.php';
+
+?>
+<section class="main-container">
+	<div class="main-wrapper">
+		<form class="login-form" action="includes/login.inc.php" method="POST">
+			<input type="text" name="uid" placeholder="Username">
+			<input type="Password" name="pwd" placeholder="Password">
+			<button type="submit" name="submit">Login</button>
+		</form>
+
+
+	</div>
+</section>
+
+<?php 
+
+// include_once 'default-footer.php';
+
+?>
---
+++ layout/header.php
@@ -0,0 +1,44 @@
+
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="public/css/main.css">
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#"></a></li>
+      <li><a href="#"> Locate Bin</a></li>
+      <li><a href="#"> Add Bin</a></li>
+      <li><a href="#"> Collect my Waste</a></li>
+      <li><a href="#"> Disposable Techniques</a></li>
+      <li><a href="#"> Upload a complaint</a></li>
+      <li><a href="#"> Notifications</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> User Profile<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Update Profile</a></li>
+          <li><a href="#">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
+  
+
+    
+
+
+</body>
+</html>
